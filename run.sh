9d1e2c85e5a60b1e6ae3e239bd0ec6be093d9805
stef-timmermans <stef.timmermans.pdx@gmail.com>
2024-11-03 17:28:10 +0100

    chore: clarify path format

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@
 # This is called by the camera control service
 
 # Activate virtual environment
+# Replace with actual path, below is an example with user fse
 source /home/fse/Documents/Projects/RPi-FSE-Truck/myvenv/bin/activate
 
 # Navigate to the working directory and start the application with GPU acceleration disabled
+# Replace with actual path, below is an example with user fse
 cd /home/fse/Documents/Projects/RPi-FSE-Truck/camera-control
 npm start -- --disable-gpu
--- single_library.sh
+++ single_library.sh
@@ -3,8 +3,10 @@
 # This is called by the camera control service
 
 # Activate virtual environment
+# Replace with actual path, below is an example with user fse
 source /home/fse/Documents/Projects/RPi-FSE-Truck/myvenv/bin/activate
 
 # Navigate to the working directory and start the gen-2-age-gender app in isolation
+# Replace with actual path, below is an example with user fse
 cd /home/fse/Documents/Projects/RPi-FSE-Truck/depthai-experiments/gen2-age-gender
 python3 main.py
