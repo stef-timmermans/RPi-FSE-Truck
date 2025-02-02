6e71e8104db2fd0ff8e8df0e3ee7ffd1dddece54
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-02-02 23:11:16 +0100

    remove unnecessary venv from single script

--- single_library.sh
+++ single_library.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# NOTE: Script assumes myvenv contains installs for the program
-
 # Navigate to the working directory and start the demo app in isolation
 # This should be done by the WorkingDirectory parameter in the service file
 # which calls this script.
 
-# Activate virtual environment
-source myvenv/bin/activate
+# No virtual environment (apparently) necessary for this demo due to
+# install configuration from ArduCam.
 
 # Go to app directory and run
 cd depthai
