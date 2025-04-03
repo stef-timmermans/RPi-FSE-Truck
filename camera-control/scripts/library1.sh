2ba908c70fc8cc1baddbbbae92c4f71650ee53c9
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-04-03 19:12:40 +0200

    handle HDMI out and zombies

--- camera-control/scripts/library1.sh
+++ camera-control/scripts/library1.sh
@@ -11,5 +11,19 @@ DEPTHAI_DIR="$SCRIPT_DIR/../../depthai"
 # Navigate to depthai (if it exists)
 cd "$DEPTHAI_DIR" || exit 1
 
-# Run the application
-python depthai_demo.py
+# Run the application in the background
+python depthai_demo.py &
+PID=$!
+
+# When this script is killed, also kill the Python child
+trap "kill $PID 2>/dev/null" SIGINT SIGTERM
+
+# Give the window a moment to appear
+sleep 5
+
+# Move the window to the HDMI display
+# Replace "host" with `wmctrl -l` to find the window name
+wmctrl -r "host" -e 0,800,0,-1,-1
+
+# Wait until the child process finishes (aka wait forever)
+wait $PID
--- camera-control/scripts/library3.sh
+++ camera-control/scripts/library3.sh
@@ -15,5 +15,19 @@ cd "$DEPTHAI_EXP_DIR" || exit 1
 # Naviage to the people counter directory
 cd gen2-people-counter || exit 1
 
-# Run the application
-python main.py
+# Run the application in the background
+python main.py &
+PID=$!
+
+# When this script is killed, also kill the Python child
+trap "kill $PID 2>/dev/null" SIGINT SIGTERM
+
+# Give the window a moment to appear
+sleep 5
+
+# Move the window to the HDMI display
+# Replace "host" with `wmctrl -l` to find the window name
+wmctrl -r "host" -e 0,800,0,-1,-1
+
+# Wait until the child process finishes (aka wait forever)
+wait $PID
