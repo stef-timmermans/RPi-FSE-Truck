311d37d092c39c48a4e4c78fc0f46f63b630ce3c
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-06-16 20:00:53 +0200

    Try different color output

--- depthai-experiments/gen2-age-gender/main.py
+++ depthai-experiments/gen2-age-gender/main.py
@@ -5,8 +5,8 @@ import cv2
 
 # Try just viewing camera input:
 with OakCamera() as oak:
-    color = oak.create_camera('color', colorSpace='rgb')
-    oak.visualize(color, callback=lambda packet: cv2.imshow("Raw", packet.frame))
+    color = oak.create_camera('color')
+    oak.visualize(color.out.main, callback=lambda packet: cv2.imshow("Raw", packet.frame))
     oak.start(blocking=True)
 
 # with OakCamera() as oak:
