d004d571010de4e61aeb698c6b41cd90753710e4
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-06-16 21:12:14 +0200

    Try adding cwd

--- camera-control/main.js
+++ camera-control/main.js
@@ -79,6 +79,7 @@ ipcMain.on('switch-mode', async (event, mode) => {
                 // Start the second camera library: Gaze Estimation
                 console.log("\nCalling script for child process #2...")
                 currentProcess = spawn(gazeEstimationEnv, [gazeEstimationPath], {
+                    cwd: gazeEstimationPath,
                     shell: true,
                     detached: true,
                     stdio: ['ignore', 'pipe', 'pipe']
